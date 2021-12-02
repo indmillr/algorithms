45d7be94dfece5f14695c96171da6525f4d6856e
Ian Miller <theiandmiller@gmail.com>
2021-12-01 21:45:33 -0600

    complete Algorithms playground

--- sandbox.js
+++ sandbox.js
@@ -90,3 +90,77 @@ function findElement(arr, func) {
   return undefined;
 }
 findElement([1, 2, 3, 4], (num) => num % 2 === 0);
+
+// Check if a value is boolean primitive
+function booWho(bool) {
+  if (typeof bool === "boolean") {
+    return true;
+  }
+  return false;
+}
+
+// Return the string with the first letter of each word capitalized
+function titleCase(str) {
+  let strArr = str.toLowerCase().split(" ");
+  let newStr = strArr.map(function (val) {
+    return val.replace(val.charAt(0), val.charAt(0).toUpperCase());
+  });
+  return newStr.join(" ");
+}
+
+// Copy each element of an array into another array, in order
+function frankenSplice(arr1, arr2, n) {
+  let copyArr = arr2.slice();
+  for (let i = 0; i < arr1.length; i++) {
+    copyArr.splice(n, 0, arr1[i]);
+    n++;
+  }
+  return copyArr;
+}
+
+// Remove all falsy values from an array
+function bouncer(arr) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i]) newArr.push(arr[i]);
+  }
+  return newArr;
+}
+
+// Return the lowest index at which a value should be inserted (if in numberical order)
+function getIndex(arr, num) {
+  arr.sort((a, b) => a - b);
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] >= num) return i;
+  }
+  return arr.length;
+}
+
+// Return true if the string in the first array element contains all the letters of the string in the second element
+function mutation(arr) {
+  let test = arr[1].toLowerCase();
+  let target = arr[0].toLowerCase();
+  for (let i = 0; i < test.length; i++) {
+    if (target.indexOf(test[i]) < 0) return false;
+  }
+  return true;
+}
+
+// Split an Array (arg1) into groups the length of (arg2) and return them as 2D Array
+function arrayInGroups(arr, size) {
+  let holder = [];
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (i % size !== size - 1) {
+      holder.push(arr[i]);
+    } else {
+      holder.push(arr[i]);
+      result.push(holder);
+      holder = [];
+    }
+  }
+  if (holder.length !== 0) {
+    result.push(holder);
+  }
+  return result;
+}
