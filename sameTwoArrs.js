a89fef37054decb6b52bbad4fc850b08fbd291d2
Ian Miller <theiandmiller@gmail.com>
2021-12-09 06:29:28 -0600

    begin sameTwoArrs

---
+++ sameTwoArrs.js
@@ -0,0 +1,23 @@
+function whatIsInAName(collection, source) {
+  const arr = [];
+
+  return arr;
+}
+
+whatIsInAName(
+  [
+    {
+      first: "Romeo",
+      last: "Montague",
+    },
+    {
+      first: "Mercutio",
+      last: null,
+    },
+    {
+      first: "Tybalt",
+      last: "Capulet",
+    },
+  ],
+  { last: "Capulet" },
+);
