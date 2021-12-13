46d5e132d8bb69e16e050cec3df549da212a0225
Ian Miller <theiandmiller@gmail.com>
2021-12-13 06:51:14 -0600

    comments

--- sumOddFibs.js
+++ sumOddFibs.js
@@ -1,12 +1,16 @@
 // Sum all Odd Fibonacci Nums <= num
 function sumFibs(num) {
+  // Variables to keep track
   let preNum = 0;
   let nowNum = 1;
   let result = 0;
+  // Make sure to stop at 'num'
   while (nowNum <= num) {
+    // if it's Odd, add it to result
     if (nowNum % 2 !== 0) {
       result += nowNum;
     }
+    // Fibonacci circle
     nowNum += preNum;
     preNum = nowNum - preNum;
   }
