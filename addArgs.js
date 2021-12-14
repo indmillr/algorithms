990f65570c14cd6f6b5bb74d9546d384f0b6317a
Ian Miller <theiandmiller@gmail.com>
2021-12-14 05:57:24 -0600

    Sum two arguments together

---
+++ addArgs.js
@@ -0,0 +1,10 @@
+// Sum two arguments together
+function addTogether() {
+  const [x, y] = arguments;
+  if (typeof x !== "number") return undefined;
+  if (y === undefined) return (y) => addTogether(x, y);
+  if (typeof y !== "number") return undefined;
+  return x + y;
+}
+
+addTogether(2, 3);
